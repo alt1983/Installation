e10cd0e16b9423f72cc268b67c7d879301b7c15b
alt1983 <timofeev_a_v@mail.ru>
2022-06-06 00:23:44 +0300

    Commit 3

--- src/Main.java
+++ src/Main.java
@@ -10,69 +10,25 @@ public class Main {
 
         StringBuilder res = new StringBuilder();
 
-        File dir1 = new File("D://Games/src");
-        File dir2 = new File("D://Games/res");
-        File dir3 = new File("D://Games/savegames");
-        File dir4 = new File("D://Games/temp");
-        File dir5 = new File("D://Games/src/main");
-        File dir6 = new File("D://Games/src/test");
-        File dir7 = new File("D://Games/res/drawables");
-        File dir8 = new File("D://Games/res/vectors");
-        File dir9 = new File("D://Games/res/icons");
-        File file1 = new File("D://Games/src/main", "Main.java");
-        File file2 = new File("D://Games/src/main", "Utils.java");
-        File file3 = new File("D://Games/temp", "temp.txt");
+        String[] dirs = {"D://Games/src", "D://Games/res", "D://Games/savegames", "D://Games/temp",
+                "D://Games/src/main", "D://Games/src/test", "D://Games/res/drawables", "D://Games/res/vectors", "D://Games/res/icons"};
+        String[][] files = {{"D://Games/src/main", "Main.java"}, {"D://Games/src/main", "Utils.java"}, {"D://Games/temp", "temp.txt"}};
 
-        if (dir1.mkdir()) {
-            res.append("Каталог src создан");
-            res.append(System.getProperty("line.separator"));
-        }
-        if (dir2.mkdir()) {
-            res.append("Каталог res создан");
-            res.append(System.getProperty("line.separator"));
-        }
-        if (dir3.mkdir()) {
-            res.append("Каталог savegames создан");
-            res.append(System.getProperty("line.separator"));
-        }
-        if (dir4.mkdir()) {
-            res.append("Каталог temp создан");
-            res.append(System.getProperty("line.separator"));
-        }
-        if (dir5.mkdir()) {
-            res.append("Каталог src/main создан");
-            res.append(System.getProperty("line.separator"));
-        }
-        if (dir6.mkdir()) {
-            res.append("Каталог src/test создан");
-            res.append(System.getProperty("line.separator"));
-        }
-        if (dir7.mkdir()) {
-            res.append("Каталог res/drawables создан");
-            res.append(System.getProperty("line.separator"));
-        }
-        if (dir8.mkdir()) {
-            res.append("Каталог res/vectors создан");
-            res.append(System.getProperty("line.separator"));
-        }
-        if (dir9.mkdir()) {
-            res.append("Каталог res/icons создан");
-            res.append(System.getProperty("line.separator"));
+        for (int i = 0; i < dirs.length; i++) {
+            File dir = new File(dirs[i]);
+            if (dir.mkdir()) {
+                res.append(("Каталог " + dirs[i] + " создан"));
+                res.append(System.getProperty("line.separator"));
+            }
         }
 
         try {
-            if (file1.createNewFile()) {
-                res.append("Файл src/main/Main.java создан");
-                res.append(System.getProperty("line.separator"));
-            }
-            if (file2.createNewFile()) {
-                res.append("Файл src/main/Utils.java создан");
-                res.append(System.getProperty("line.separator"));
-
-            }
-            if (file3.createNewFile()) {
-                res.append("Файл temp/temp.txt создан");
-                res.append(System.getProperty("line.separator"));
+            for (int i = 0; i < files.length; i++) {
+                File file = new File(files[i][0], files[i][1]);
+                if (file.createNewFile()) {
+                    res.append(("Файл " + files[i][0] + "/" + files[i][1] + " создан"));
+                    res.append(System.getProperty("line.separator"));
+                }
             }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
@@ -89,5 +45,3 @@ public class Main {
 
     }
 }
-
-
